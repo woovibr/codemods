883577846a6b1f72a0fa9b3306360ad52eef0cff
Fernando Silva <fernando457829@gmail.com>
2021-12-29 16:14:57 -0300

    test(moveFileToPackage): fix tempy dependency

--- packages/moveFileToPackage/jest.config.js
+++ packages/moveFileToPackage/jest.config.js
@@ -2,4 +2,6 @@ const baseOptions = require('../../jest.config.base');
 
 module.exports = {
   ...baseOptions,
+
+  transformIgnorePatterns: ['node_modules/(?!tempy|unique-string|crypto-random-string|is-stream)'],
 };
